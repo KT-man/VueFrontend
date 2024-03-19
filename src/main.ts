9e5a6dddf1a8b4d592a9c39fcac8825d81d22d06
kai.ting.man <kai.ting.man@accenture.com>
2024-03-19 22:57:28 +0800

    Update to use Vuetify, slowly start build

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives
+})
 
 const app = createApp(App)
 
+app.use(vuetify)
 app.use(createPinia())
 app.use(router)
 
